c67ef271e1b9a76e1df3cf3568941a37ca63649e
BenjaminWolfgang <BenjaminWolfgang@users.noreply.github.com>
2020-01-06 01:27:42 -0600

    Adding Project

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerControls : MonoBehaviour
+{
+    public float speed, jumpForce;
+    private Rigidbody2D myRB;
+    private bool grounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        myRB.gravityScale = 2;
+        myRB.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        myRB.velocity = new Vector2(h * speed, myRB.velocity.y);
+
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            myRB.velocity = Vector2.up * jumpForce;
+            grounded = false;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+}
