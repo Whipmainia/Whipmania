660764a5b7dc275ac5888db44116b94146c4b3b5
TreyWild <37952876+TreyWild@users.noreply.github.com>
2020-03-01 21:21:55 -0600

    Enemy Horizontal Movement

---
+++ Assets/Scripts/EnemyControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControls : MonoBehaviour
+{
+    public float enemySpeed, horizontalMovement = 0f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalMovement * enemySpeed, rb.velocity.y);
+    }
+}
